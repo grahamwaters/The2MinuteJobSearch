1e68089e8b08b00b53015b6c7ba1b29e6ba86e64
Graham Waters <graham.waters.business@gmail.com>
2022-11-16 16:06:18 -0600

    selenium: Updated

--- scripts/linkedin_automation.py
+++ scripts/linkedin_automation.py
@@ -56,17 +56,103 @@ def login(secrets):
 
 #* Action Function for LinkedIn
 # @limit(1, 60) # limit the function to 1 call per 60 seconds
-def harvest(company_url,lamp_df,driver):
+def harvest(company_url,lamp_df,driver,url_patterns):
     # we are logged in to LinkedIn at this point
     # get the company page
-    css_tag1 = 'driver.find_elements_by_css_selector("#main > div.org-grid__content-height-enforcer > div > div.artdeco-card.pb2 > div.display-flex.full-width.justify-space-between.align-items-center.pt5.ph5")[0].get_attribute'
+    """
+    harvest the company page for the company_url provided and update the lamp_df dataframe with the results, which should be the information for the hiring managers at the company as listed on the company page (company_url).
+
+    :param company_url: the url for the company page
+    :type company_url: str
+    :param lamp_df: the dataframe to update with the results
+    :type lamp_df: pandas.DataFrame
+    :param driver: the selenium driver
+    :type driver: selenium.webdriver.chrome.webdriver.WebDriver
+    :return: the updated dataframe
+    :rtype: pandas.DataFrame
+    """
+    # css_tag1 = 'driver.find_elements_by_css_selector("#main > div.org-grid__content-height-enforcer > div > div.artdeco-card.pb2 > div.display-flex.full-width.justify-space-between.align-items-center.pt5.ph5")[0].get_attribute'
+
+    # scrolling options for random choice
+    scroll_options = ["window.scrollTo(0, document.body.scrollHeight/2);","window.scrollTo(0, document.body.scrollHeight/4);","window.scrollTo(0, document.body.scrollHeight/4*3);","window.scrollTo(0, document.body.scrollHeight);"]
+
+    #& go to the company page
     driver.get(company_url) # get the company page
     # wait for the people you may know section to load
     WebDriverWait(driver, 90).until(EC.presence_of_element_located((By.CLASS_NAME,"org-grid__content-height-enforcer")))
     time.sleep(5) # sleep for 5 seconds
     # get the list of people you may know
+
+    # scroll down incrementally (using some randomness) to emulate human behavior until you reach the bottom of the page.
+    # get the height of the page
+    last_height = driver.execute_script("return document.body.scrollHeight")
+    iterations = 0 # keep track of the number of iterations to prevent infinite loops
+    while True:
+        inner_command = random.choice(scroll_options)
+        driver.execute_script(inner_command) # scroll down
+        # wait to load the page
+        time.sleep(random.randint(1, 3)) # sleep for 1 to 3 seconds
+        # calculate new scroll height and compare with last scroll height
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height or iterations == 5: # if the page has reached the bottom, break the loop and continue
+            break
+        iterations += 1
+        last_height = new_height
+
+    print('Scrolling complete, now looking for people on the page')
+    # get the list of people you may know
+    people = driver.find_elements_by_css_selector(".org-people-profile-card__profile-title.t-black.t-bold")
+
+    # get the buttons on the page to click
+    button_class = url_patterns['buttons'] # get the button class
+    # check the inner text of the button to see if it is "Connect"/"Message" or "Follow" or "Pending"
+    buttons = driver.find_elements_by_css_selector(button_class) # get the buttons
+    last_connect_time = time.time() # keep track of the last time you connected to prevent spamming the connect button
+    # loop through the people and connect with them
+    for button in buttons:
+        if button.text == "Follow":
+            button.click()
+            time.sleep(random.randint(1, 3))
+            # detect the confirm button and click it
+            detect_confirm_button(driver)
+            time.sleep(random.randint(1, 3))
+        elif button.text == "Connect": # if the button says "Connect" then we only click it if we have not clicked more than 3 times in the last 5 minutes
+            # get the current time
+            current_time = time.time()
+            # check if the current time is within 5 minutes of the last time we clicked the connect button
+            if current_time - last_connect_time > 300: # if it has been more than 5 minutes since the last time we clicked the connect button
+                button.click()
+                time.sleep(random.randint(1, 3))
+                # detect the confirm button and click it
+                detect_confirm_button(driver)
+                time.sleep(random.randint(1, random.randint(3, 5)))
+                # update the last_connect_time
+                last_connect_time = current_time
+            else: # if it has been less than 5 minutes since the last time we clicked the connect button
+                continue
+        elif button.text == "Message": # if the button says "Message" then we only click it if we have not clicked more than 3 times in the last 5 minutes
+            pass # we will not click the message button
+
+        elif button.text == "Pending": # if the button says "Pending" then we only click it if we have not clicked more than 3 times in the last 5 minutes
+            pass # we will not click the pending button
+        else:
+            pass
+
+
+
+
+
     people_you_may_know = driver.find_elements_by_css_selector(".org-people-profiles-module__profile-list") # get the list of people you may know
     # these elements are not clickable, so we need to get the url from the href attribute
+
+    # either people or people_you_may_know will be empty, but not both
+    if len(people) > 0: # if there are people on the page
+        people_you_may_know = people
+    elif len(people_you_may_know) > 0: # if there are people you may know on the page
+        pass
+    else: # if there are no people or people you may know on the page
+        people_you_may_know = [] # set the people_you_may_know to an empty list
+
     driver.find_elements_by_css_selector("#main > div.org-grid__content-height-enforcer > div > div.artdeco-card.pb2 > div.display-flex.full-width.justify-space-between.align-items-center.pt5.ph5")[0].get_attribute('href')
     # get the list of urls for the people you may know
     people_you_may_know_urls = [person.find_element_by_css_selector("a").get_attribute("href") for person in people_you_may_know]
@@ -153,8 +239,8 @@ def process_flow():
         company_url = pattern_url.format(str(company).lower()) # create the url for the company
         print(f'Company URL: {company_url}')
         print(f'Company Name: {company}')
-        # harvest the data for the company
-        lamp_df = harvest(company_url,lamp_df,driver)
+        # harvest the data for the company and update the lamp_df dataframe
+        lamp_df = harvest(driver,company_url,lamp_df,url_patterns)
         print(f' -- Completed harvesting for {company} --')
         # sleep for a random amount of time
         time.sleep(random.randint(1,3)) # sleep for a random amount of time

97374a69eb16aba2c51b2364534060de9c0400ca
Graham Waters <graham.waters.business@gmail.com>
2022-11-16 21:06:08 -0600

    removed unused imports

--- scripts/linkedin_automation.py
+++ scripts/linkedin_automation.py
@@ -16,9 +16,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
 
 # import Keys
-from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import (
-    NoSuchElementException,
     ElementClickInterceptedException,
 )
 

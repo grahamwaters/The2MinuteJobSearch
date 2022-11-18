af2b7fe041062393a935ccb10a80a6eb48d4d1bd
Graham Waters <graham.waters.business@gmail.com>
2022-11-18 16:08:56 -0600

    just-lamp-generator: Updated

---
+++ scripts/Untitled-1.py
@@ -0,0 +1,267 @@
+"""
+
+
+Template 2: Find your career crushes and ask them to coffee.
+Hi NAME!
+My name is X and I am a current X Immersive student at General Assembly. I'm looking to learn more about the tech field in CITY. I was really impressed by your career path and would love to learn about how you got to where you are now. Would you be open to coffee (my treat) or a quick phone call in the next couple of weeks?
+Thanks!
+NAME
+
+Template 3: Asking someone to connect you
+Hi NAME!
+I hope everything is going well with you! As you may know, I've just started General Assembly's X immersive, and I'm loving it so far! I'll soon be job searching and am looking to learn more about the tech field in CITY. I saw that you are connected to ABC and I'm very interested in that organization. Would you be willing to connect us? I've written an email below that you can copy and paste:
+X
+Thanks!
+NAME
+
+Template 4: Message for recruiter while in training
+Dear NAME,
+My name is X, and I am a X student at General Assembly. Today, I might not have the skills that you are looking for, but someday soon, I will. If you are amenable, I'd like to establish a relationship with you, so when that day comes, you can get credit for my hire. If it's at all possible, I would love to give your [coding test] a shot now, just to find out where I am. If not, let me know the key skills you are looking for and I will [go to Github, etc.] and build out my contributions on those skill sets. I appreciate your time."
+Thanks,
+NAME
+
+Template 5: While watching a webinar:
+Hi NAME!
+I’m tuning into the (panel) event and love the advice you have for new UXers like myself. I would love to stay connected on LinkedIn. Would you be available for a quick 15 minute chat in the coming weeks so I can learn more about your UX journey?
+Thanks,
+NAME
+
+Template 6: After watching a webinar:
+Hi NAME!
+I watched last week’s (event) panel and am inspired by your not-so-linear path into UX research. Would you be available to connect some time in the next 2 weeks to chat about your experience? I am usually available any time after X. Thank you in advance!
+NAME
+
+Template 7: Cold outreach to director of a company:
+Hi NAME!
+My name is [NAME] and I am an aspiring UX researcher. As a longtime user of (app), I would love to hear about your day-to-day at the company and how the pandemic has impacted your research! Would you be free in the next 2 weeks to talk about your experience? Looking forward to connecting!
+NAME
+
+Template 8: Following up after you met them at an event
+Hello NAME,
+It was great to meet you at X EVENT yesterday. I appreciated your insights into [SPECIFIC FACT YOU TALKED ABOUT]. I would love to continue the conversation. Would you be open to coffee (my treat) or a quick phone call in the next couple of weeks?
+I look forward to hearing from you!
+Thank you,
+NAME, EMAIL, PHONE, RESUME
+
+Template 9: Following Up after you have applied to a role
+Hello X ORG HIRING TEAM,
+
+I recently applied to X Role and wanted to be sure you had everything you needed to move forward with your hiring process.  I am very excited about ways we could partner together to make an impact on the X Team.
+
+I look forward to hearing from you.
+
+Thank you,
+
+NAME, EMAIL, PHONE, RESUME
+
+Template 10: Following up after an informational interview
+Hello NAME,
+
+It was great meeting you! Thank you for sharing your insights on [COMPANY NAME]’s culture and your experiences as [JOB TITLE].
+
+As we discussed, I’m all about supporting folks in getting to their goals, so let me know when you’re ready, and I’ll send an email to [CONTACT OF INTEREST].
+
+And thank you again for referring me! My resume is attached, and I’ll keep you updated on my progress.
+
+Take good care,
+
+NAME, EMAIL, PHONE, RESUME
+
+Template 11: Following up after a first interview
+Hello NAME,
+
+Thank you! It was great meeting you and the team at [COMPANY NAME] about the [JOB TITLE] role. Your team is clearly committed to fast-paced performance, constant improvement, and having a good time. My two years in [FIELD DESCRIBED IN INTERVIEW] helped me decide before attending GA that a company like yours would be my next employer.
+
+I can’t wait to hear about next steps in the process.
+
+All the best,
+
+NAME, EMAIL, PHONE, RESUME
+
+Template 12: Following up after receiving an offer
+Hello NAME,
+
+Thank you again for this offer. I’m so excited about the possibility of joining the team. I want to consider this offer in detail. When do you need a response from me?
+
+I look forward to following up with you, and again, thank you!
+
+All the best,
+
+NAME, EMAIL, PHONE, RESUME
+
+Template 13: Following up to negotiate
+Hello NAME,
+
+Thank you so much for everything you and your team have done through this process.  I really appreciate the focus on x, y, and z.
+
+After having some time to review the offer package, I wanted to make the ask of increasing the current salary amount of $x to $y.  I am focused on the entirety of the package, and based on my abilities to make an impact using my x,y,z, skills and my commitment to making this a smooth transition for all involved, I am making this request.
+
+I look forward to hearing from you!
+
+Thank you,
+
+NAME, EMAIL, PHONE, RESUME
+
+Template 14: When it appears they have ghosted you
+Hello NAME,
+
+I know it has been some time since we have communicated, but I did want to check in and see if there has been any movement on the decision for {X Role}.  I am still very interested in contributing to the team and the organization and think we could do great work together. Please let me know if there is anything I can do or provide to help make your decision.
+
+I look forward to hearing from you!
+
+Thank you,
+
+NAME, EMAIL, PHONE, RESUME
+
+Template 15: Connecting after reading an article: (source)
+Hi NAME!
+
+I came across a LinkedIn article you wrote on [the fundamentals of product management]. I really resonated with your thoughts on how [data should be at the core of every product decision.]
+
+I'm an X. I'd love to keep in touch and learn more about your work.
+
+NAME
+
+Template 16: Connecting with someone in a group you’re in: (source)
+Hi NAME,
+
+I came across your profile in the [Consulting Group on LinkedIn]. Your post recently about XYZ really resonated with me, so I thought I'd connect with you to keep in touch and maybe bounce ideas off each other at some point in the future!
+
+All the best!
+NAME
+
+Template 17: Asking for a LinkedIn Recommendation
+Hi [Name],
+
+I hope you’re having a great week!
+
+I want to let you know how much I enjoy working with you, as well as how much I value your insights and feedback. I’ve especially enjoyed collaborating with you on [project you worked on together].
+
+I have a small request for you. I aim to keep my LinkedIn profile updated to provide an accurate picture of my skills and experiences. With that in mind, I’d love if you could write me a LinkedIn recommendation that highlights my skills in [area] and [area].
+
+I’d be happy to write you a recommendation in return. Just let me know if there’s something specific you’d like me to call attention to.
+
+Would you feel comfortable writing a recommendation of this kind for me? Of course, no pressure either way.
+
+All the best,
+[Your Name]
+
+Template 18: Messaging a recruiter to find another recruiter
+
+Hi NAME!
+My name is X, and I am an X. I am interested in learning more about the X role [requisition #] at X company. Do you know who the technical recruiter seeking talent for this opening is?
+Thank you!
+NAME
+
+
+Template 19: Following up (3-5 days later)
+Hi again!
+Just following up on my last message - are you available to chat in the next couple weeks?
+NAME
+
+
+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+7 Questions You Can't Leave an Informational Interview Without Asking (The Muse)
+
+What Attracted You to This Career Path?
+What Previous Professional Experiences Have Helped You Most in This Role?
+What’s Something That Would Surprise People About Your Day-to-Day?
+What’s One Thing You Wish Somebody Would’ve Told You Before Going Into This Field?
+What Are Some of the Biggest Rewards of Your Position?
+How Would You Describe Somebody Who Would Excel in This Career?
+What’s Most Important to Prepare for a Role Like Yours?
+
+
+Tips from LinkedIn Learning Instructor/Recruiter Jenny Foss
+
+General Tips
+Where do I find them? LinkedIn, Google, through GA grads or mutual connections
+Time is money for recruiters. Respond/follow up quickly.
+A great recruiter can be an ally to you, and they have every incentive for you to do well in the process, whether it’s an internal or external recruiter
+Being ghosted by a recruiter often has nothing to do with you. Why might this happen? Feedback from the client is slow, they are still gauging excitement from the rest of the team, they have other project priorities, there’s internal candidate interest, a change in job scope, or they are no longer with the company. If you are ghosted, follow up asking about the next steps. If they do not respond, call them. Work on other opportunities in the meantime. Send one final email before moving on.
+
+Conversations with Internal Recruiters:
+Hi, I noticed your job posting and I think I might be a good match. May I ask you a couple of quick questions?
+What’s your process?
+How often should we communicate?
+How do you prefer I communicate with you?
+Realistically, how strong of a candidate am I?
+Who will I be interviewing with, and what can I expect?
+What's the format of the interview?
+Do you have tips on how to put my best foot forward?
+What do you recommend I bring or prepare?
+What’s the dress code?
+
+Conversations with External Recruiters:
+Hi, I'm a X and noticed your firm represents clients in my field. I wonder if it makes sense for us to chat?
+
+Do NOT ask/say:
+"I'd like to add you to my professional network on LinkedIn."
+How many people are interviewing?
+How do I stack up against the competition?
+Can you pull a few strings for me?
+
+When your recruiter isn’t a good fit (source)
+“Whatever the case may be, if you're feeling like it's just not happening between the two of you, you don't have to feel like you're stuck with that person. And in fact, that could really hinder your progress if you do. Instead, I recommend that you respond. And I would follow along with these steps.
+Number one, just be honest with them and upfront, but not combative. Maybe you let them know how you're feeling. "Hey, I really appreciate your interest in me, "but I feel like maybe something's not clicking. "Are you feeling the same way?"
+Number two, share a little bit of constructive feedback, and give 'em an opportunity to respond. If they're open to that feedback and able to course correct, you've solved your problem right there.
+Now, sometimes you're feeling like you're really stuck. What do you do then? What I would do then, is call the main office of that recruiting agency and ask for the branch manager. When you get on the phone, I would say something like, "Look, I'm really excited to be working with you guys, "but I feel like maybe I don't have the right recruiter "for me, and here's why. "What do you recommend I do?" I can guarantee you that if that branch manager feels like you're a good fit for the clients that they're working with, they're going to figure out a solution really fast, whether that's have a conversation with that recruiter or assign you elsewhere. Now I recognize, it's probably not going to be a lot of fun to have these conversations. But what is worse? Do you want to feel like you're stuck with somebody who is not going to be supportive to your job search? Or have a constructive conversation that might help you resolve things fairly quickly, and bring that person back on board as your ally or get somebody else who's going to help you take this the distance.”
+
+––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––––
+
+
+Talking to Recruiters
+
+Questions:
+Do you work for the hiring company? (are you internal or external?)
+How quickly are you looking to fill this position?
+What's the starting salary range?
+Who will be interviewing me?
+What types of interview questions shall I expect?
+How long has the position been open? Is this position a backfill or newly created?
+What can you tell me about the hiring manager?
+What are the three or four most important skills?
+What do the day-to-day responsibilities of the role look like?
+What are the company’s values? What characteristics do you look for in employees in order to represent those values?
+What’s your favorite part about working at the company?
+What does success look like in this position, and how do you measure it?
+Are there opportunities for professional development?  If so, what do those look like?
+Who will I be working most closely with?
+What do you see as the most challenging aspect of this job?
+Is there anything about my background or resume that makes you question whether I am a good fit for this role?
+What are the next steps?
+
+Resources
+6 Things to Expect When Talking to a Recruiter
+How to Talk to a Recruiter for the First Time: 18 Useful Tips
+9 Good Questions to Ask Recruiters
+The Do’s and Don’ts of Working With Recruiters
+How To Use LinkedIn To Get Technical Recruiters To Offer You Interviews
+
+
+
+
+Hi Liz,
+My name is X, and I am a software engineer in training. I spoke with a few WWF staff about the X opening (req.). Do you know who the recruiter for this posting is? if so, I'd love to ask them a few questions.
+Thanks so much,
+X"""
+
+PLACE_HOLDER_ONE = '[your career trajectory AND/OR experience working at X company AND/OR the culture of X team AND/OR the types of projects you’re working on]'
+informational_interview_prospect_general = """Hi NAME!
+I saw your post about WHATTHEYPOSTEDABOUT. I found this interesting and wanted to reach out. I saw you work at THEIRCOMPANY, and I’d love to learn more about PLACE_HOLDER_ONE. Do you have 15 mins in the next couple weeks to chat?
+Thanks!
+NAME"""
+
+coffee_with_career_crushes = """Hi NAME!
+My name is YOURNAME, and I am a YOURJOBTITLE in training. I spoke with a few THEIRCOMPANY staff about the JOBTITLE opening (req.). Do you know who the recruiter for this posting is? if so, I'd love to ask them a few questions.
+Thanks so much,
+YOURNAME"""
+
+asking_someone_to_connect_you = """Hi NAME! I hope everything is going well with you! As you may know, I've been studying TOPIC lately and actively working with projects in the space. I'll soon be job searching and am looking to learn more about the tech field in CITY. I saw that you are connected to THEIRCOMPANY and I'm very interested in that organization. Would you be willing to connect us? I've written an email below that you can copy and paste: EMAIL_TEMPLATE \n Thanks! \n YOURNAME"""
+
+cold_outreach_to_company_director = """Hi NAME!\n My name is YOURNAME and I am an aspiring JOBTITLE. As a longtime user of PRODUCT_YOU_BOTH_USE, I would love to hear about your day-to-day at THEIRCOMPANY and how the pandemic has impacted your research! Would you be free in the next 2 weeks to talk about your experience? Looking forward to connecting!\n YOURNAME"""
+
+followup_after_event = """Hello NAME,\n It was great to meet you at EVENTNAME. I appreciated your insights into WHATYOUTALKEDABOUT. I would love to continue the conversation. Would you be open to coffee (my treat) or a quick phone call in the next couple of weeks?\n I look forward to hearing from you!\n YOURNAME, YOUREMAIL, YOURPHONE, YOURRESUMELINK"""
+
